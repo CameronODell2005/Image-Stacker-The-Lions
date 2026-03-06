6ffbc5dee77426e174c65c18718d70e21217a575
CameronODell2005 <codell8@ut.utm.edu>
2026-03-06 20:15:57 +0000

    Initial setup

---
+++ main.cpp
@@ -0,0 +1,9 @@
+/*
+    main.cpp
+    Group Name: The Lions
+    Group Members: Brandon Daniel and Cameron O'Dell
+    Course: CSCI325
+    Description: 
+
+*/
+
---
+++ stacker.cpp
@@ -0,0 +1,8 @@
+/*
+    stacker.cpp
+    Group Name: The Lions
+    Group Members: Brandon Daniel and Cameron O'Dell
+    Course: CSCI325
+    Description: 
+
+*/
